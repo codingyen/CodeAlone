cd4d92f329f6e3ac36c8ceaee03b64cc07b34195
codingyen <chihhoyen@gmail.com>
2018-08-20 17:10:07 -0700

    Add the complexity for 253.

--- Python/253_meeting_room_ii.py
+++ Python/253_meeting_room_ii.py
@@ -1,6 +1,9 @@
 """
 Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei)
 find the minimum number of conference rooms required
+
+Time:  O(nlogn) because of the sort
+Space: O(n)     space to store the start and end times
 """
 
 class Interval(object):
