bb6216ebb57c311ea5c95653aadda557b4d8f31a
codingyen <7268404+codingyen@users.noreply.github.com>
2020-08-05 22:46:15 -0700

    test

---
+++ Project/test.py
@@ -0,0 +1,28 @@
+class Thing(object):
+    """物品"""
+
+    def __init__(self, name, price, weight):
+        self.name = name
+        self.price = price
+        self.weight = weight
+
+    @property
+    def value(self):
+        """价格重量比"""
+        return self.price / self.weight
+
+
+def input_thing():
+    """输入物品信息"""
+    name_str, price_str, weight_str = input().split()
+    return name_str, int(price_str), int(weight_str)
+
+
+def main():
+    string1 = "123"
+    string2 = "456"
+    print(string1, string2)
+
+
+if __name__ == '__main__':
+    main()
