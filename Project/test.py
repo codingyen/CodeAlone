d42e80146807109918d198e693b4feeae778a411
codingyen <7268404+codingyen@users.noreply.github.com>
2020-08-07 16:08:14 -0700

    Just update.

--- Project/test.py
+++ Project/test.py
@@ -1,28 +1,21 @@
-class Thing(object):
-    """物品"""
+import math
+import os
+import random
+import re
+import sys
 
-    def __init__(self, name, price, weight):
-        self.name = name
-        self.price = price
-        self.weight = weight
-
-    @property
-    def value(self):
-        """价格重量比"""
-        return self.price / self.weight
-
-
-def input_thing():
-    """输入物品信息"""
-    name_str, price_str, weight_str = input().split()
-    return name_str, int(price_str), int(weight_str)
-
-
-def main():
-    string1 = "123"
-    string2 = "456"
-    print(string1, string2)
 
 
 if __name__ == '__main__':
-    main()
+    n = 5
+    b = bin(n)
+    maxcon = 0
+    curr = 0
+    for i in range(2, len(b)):
+        if int(b[i]) == 1:
+            curr += 1
+        else:
+            curr = 0
+        maxcon = max(maxcon, curr)
+
+    print(maxcon)
