b689c87b7ab0a17409d2c185e0b787329903ade2
codingyen <7268404+codingyen@users.noreply.github.com>
2020-08-09 11:08:57 -0700

    Solution of 0273.

---
+++ Python/0273_integer_to_english_words.py
@@ -0,0 +1,91 @@
+class Solution:
+    def numberToWords(self, num):
+        def one(num):
+            lookup = { 
+                1: 'One', 
+                2: 'Two', 
+                3: 'Three', 
+                4: 'Four', 
+                5: 'Five',
+                6: 'Six',
+                7: 'Seven',
+                8: 'Eight',
+                9: 'Nine'
+            }
+            return lookup[num]
+
+        def less_20(num):
+            lookup = {
+                10: 'Ten',
+                11: 'Eleven',
+                12: 'Twelve',
+                13: 'Thirteen',
+                14: 'Fourteen',
+                15: 'Fifteen',
+                16: 'Sixteen',
+                17: 'Seventeen',
+                18: 'Eighteen',
+                19: 'Nineteen',
+            }
+            return lookup[num]
+        
+        def ten(num):
+            lookup = {
+                2: 'Twenty',
+                3: 'Thirty',
+                4: 'Forty',
+                5: 'Fifty',
+                6: 'Sixty',
+                7: 'Seventy',
+                8: 'Eighty',
+                9: 'Ninety'
+            }
+            return lookup[num]
+
+        def two(num):
+            if not num:
+                return ''
+            elif num < 10:
+                return one(num)
+            elif num < 20:
+                return less_20(num)
+            else:
+                tenner = num // 10
+                rest = num - tenner * 10
+                return ten(tenner) + ' ' + one(rest) if rest else ten(tenner)
+        
+        def three(num):
+            hundred = num // 100
+            rest = num - hundred * 100
+            if hundred and rest:
+                return one(hundred) + ' Hundred ' + two(rest)
+            elif not hundred and rest:
+                return two(rest)
+            elif hundred and not rest:
+                return one(hundred) + ' Hundred'
+
+        billion = num // 1000000000
+        million = (num - billion * 1000000000) // 1000000
+        thousand = (num - billion * 1000000000 - million * 1000000) // 1000
+        rest = num - billion * 1000000000 - million * 1000000 - thousand * 1000
+        if not num:
+            return 'Zero'
+        result = ''
+        if billion:
+            result = three(billion) + ' Billion'
+        if million:
+            result += ' ' if result else ''
+            result += three(million) + ' Million'
+        if thousand:
+            result += ' ' if result else ''
+            result += three(thousand) + ' Thousand'
+        if rest:
+            result += ' ' if result else ''
+            result += three(rest)
+
+        return result
+
+if __name__ == '__main__':
+    s = Solution()
+    num = 12345
+    print(s.numberToWords(num))
