36442726d321c9b2c80cb3d7530c317651b0654f
codingyen <7268404+codingyen@users.noreply.github.com>
2020-09-02 16:25:51 -0700

    Add another solution for 0973.

--- Python/0973_k_closest_points_to_origin.py
+++ Python/0973_k_closest_points_to_origin.py
@@ -20,6 +20,27 @@ class Solution:
         points.sort(key = lambda x: x[0] ** 2 + x[1] ** 2)
         return points[:K]
 
+# Solution 3 Divide and Conquer
+        """
+        dist = lambda i: points[i][0]**2 + points[i][1]**2
+
+        def work(i, j, K):
+            if i >= j: return
+            oi, oj = i, j
+            pivot = dist(random.randint(i, j))
+            while i < j:
+                while i < j and dist(i) < pivot: i += 1
+                while i < j and dist(j) > pivot: j -= 1
+                points[i], points[j] = points[j], points[i]
+
+            if K <= i - oi + 1:
+                work(oi, i, K)
+            else:
+                work(i+1, oj, K - (i - oi + 1))
+
+        work(0, len(points) - 1, K)
+        return points[:K]
+        """
 
 
 if __name__ == '__main__':
