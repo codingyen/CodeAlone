e3d60cfb0c964667d34eb390c2bcdd616e5356dc
codingyen <7268404+codingyen@users.noreply.github.com>
2020-09-02 16:24:52 -0700

    Add comment for 0003.

--- Python/0003_longest_substring_without_repeating_characters.py
+++ Python/0003_longest_substring_without_repeating_characters.py
@@ -1,3 +1,10 @@
+# Hint1: Use Hash Table.
+# Hint2: Create left, maxlen variables.
+# Hint3: Iterate through all the string.
+
+# Time: O(n)
+# Space: O(1)
+
 class Solution:
     def lengthOfLongestSubstring(self, s):
         lookup = {}
